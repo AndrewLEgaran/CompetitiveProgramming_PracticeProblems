d5f8d48dc9f19b670fae86ff2f0ffa29a2b5b9c9
AndrewLEgaran <andrewlester.egaran@smc.pshs.edu.ph>
2025-11-07 13:17:54 +0800

    Multiplication table for the number 4

---
+++ Activity2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 4;
+    int prod = 0;
+    for (int y = 1; y <= 10; y++) {
+        prod = x*y;
+        cout << x << " x " <<  y << " = " << prod << "\n";
+}
+}
